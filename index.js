c9300eb1956ab2d7367a9cc39370b48291f328e5
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2020-12-13 23:30:51 +0100

    try again

---
+++ index.js
@@ -0,0 +1,12 @@
+function goOnline() {
+  var x = document.getElementById("online");
+  var y = document.getElementById("offline");
+  x.style.display = "block";
+  y.style.display = "none";
+}
+function goOffline() {
+  var x = document.getElementById("online");
+  var y = document.getElementById("offline");
+  y.style.display = "block";
+  x.style.display = "none";
+}
